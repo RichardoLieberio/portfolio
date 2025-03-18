36e253490104f814e706e3be668a268f8755066d
Richardo Lieberio <richardo.alt@gmail.com>
2025-03-18 11:26:13 +0700

    Added poppins font and modified layout styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
+import { NextFont } from 'next/dist/compiled/@next/font';
+import { Poppins } from 'next/font/google';
 import { ReactNode, JSX } from 'react';
 import './globals.css';
+
+const poppins: NextFont = Poppins({
+    weight: [ '100', '200', '300', '400', '500', '600', '700', '800', '900' ],
+    style: [ 'normal', 'italic' ],
+    subsets: [ 'latin' ],
+});
 
 type AppLayoutProps = {
     menu: ReactNode;
@@ -15,8 +23,8 @@ type AppLayoutProps = {
 export default function AppLayout({ menu, home, work, experience, services, about, stats, contact }: AppLayoutProps): JSX.Element {
     return (
         <html lang="en" className="scroll-smooth">
-            <body className="w-full min-w-80 h-dvh relative text-base overflow-hidden">
-                <main className="h-full px-4 sm:px-6 overflow-y-auto scroll-smooth scrollbar-none">
+            <body className={`${poppins.className} w-full min-w-80 h-dvh relative text-sm sm:text-base tracking-tight overflow-hidden`}>
+                <main className="max-w-7xl h-full mx-auto px-4 sm:px-6 relative overflow-y-auto scroll-smooth scrollbar-none">
                     { home }
                     { work }
                     { experience }
