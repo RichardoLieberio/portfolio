9cf72219bf7363f9de3a09a8af5380ea63ac499c
Richardo Lieberio <richardo.alt@gmail.com>
2025-03-18 14:23:17 +0700

    Modified css to hide body's scrollbar, layout is scrollable from anywhere, menu position no longer changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,8 @@ type AppLayoutProps = {
 export default function AppLayout({ menu, home, work, experience, services, about, stats, contact }: AppLayoutProps): JSX.Element {
     return (
         <html lang="en" className="scroll-smooth">
-            <body className={`${poppins.className} w-full min-w-80 h-dvh relative text-sm sm:text-base tracking-tight overflow-hidden`}>
-                <main className="max-w-7xl h-full mx-auto px-4 sm:px-6 relative overflow-y-auto scroll-smooth scrollbar-none">
+            <body className={`${poppins.className} w-full min-w-80 text-sm sm:text-base tracking-tight`}>
+                <main className="max-w-7xl mx-auto px-4 sm:px-6 ">
                     { home }
                     { work }
                     { experience }
