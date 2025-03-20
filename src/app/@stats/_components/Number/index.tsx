941d2ff43461cbc7b8bf9f0409a0a6bb866003df
Richardo Lieberio <richardo.alt@gmail.com>
2025-03-20 16:33:22 +0700

    Stats number will reanimate when in viewport

--- src/app/@stats/_components/Number/index.tsx
+++ src/app/@stats/_components/Number/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useState, useEffect, JSX, Dispatch, SetStateAction } from 'react';
-import { motion, animate, useMotionValue, useTransform, Easing, MotionValue, AnimationPlaybackControls } from 'framer-motion';
+import { useState, useRef, useEffect, JSX, Dispatch, SetStateAction, RefObject } from 'react';
+import { motion, animate, useMotionValue, useTransform, useInView, Easing, MotionValue, AnimationPlaybackControls } from 'framer-motion';
 
 type NumberProps = {
     number: number;
@@ -14,18 +14,25 @@ export default function Number({ number, duration, ease }: NumberProps): JSX.Ele
     const count: MotionValue<number> = useMotionValue(0);
     const rounded: MotionValue<number> = useTransform(count, (latest) => Math.round(latest));
 
-    useEffect(() => {
-        const controls: AnimationPlaybackControls = animate(count, number, { duration, ease, type: 'tween' });
-        const unsubscribe: VoidFunction = rounded.on('change', (val) => setDisplayNumber(val));
+    const ref: RefObject<HTMLSpanElement | null> = useRef<HTMLSpanElement>(null);
+    const inView: boolean = useInView(ref, { once: false, margin: '0px 0px -50px 0px' });
 
-        return (): void => {
-            controls.stop();
-            unsubscribe();
-        };
-    }, [ number, duration, ease, count, rounded ]);
+    useEffect(() => {
+        if (inView) {
+            count.set(0);
+
+            const controls: AnimationPlaybackControls = animate(count, number, { duration, ease, type: 'tween' });
+            const unsubscribe: VoidFunction = rounded.on('change', (val) => setDisplayNumber(val));
+
+            return (): void => {
+                controls.stop();
+                unsubscribe();
+            };
+        }
+    }, [ number, duration, ease, count, rounded, inView ]);
 
     return (
-        <motion.span className="text-4xl md:text-5xl font-semibold" aria-live="polite" aria-label={ `Count: ${displayNumber}` }>
+        <motion.span ref={ ref } className="text-4xl md:text-5xl font-semibold" aria-live="polite" aria-label={ `Count: ${displayNumber}` }>
             { displayNumber }+
         </motion.span>
     );
