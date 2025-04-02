aa527c2b59f311297536b76e686b5912f938227e
Richardo Lieberio <richardo.alt@gmail.com>
2025-04-02 10:05:21 +0700

    Work section is done

--- src/app/@works/_components/ProjectCard/index.tsx
+++ src/app/@works/_components/ProjectCard/index.tsx
@@ -6,14 +6,14 @@ type ProjectCardProps = {
     src: string;
     alt: string;
     project: string;
-    categories: string[];
     title: string;
     description: string;
+    stack: { logo: string, name: string, small?: boolean }[]
     live: string;
     github: string;
 };
 
-export default function ProjectCard({ src, alt, project, categories, title, description, live, github }: ProjectCardProps): JSX.Element {
+export default function ProjectCard({ src, alt, project, title, description, stack, live, github }: ProjectCardProps): JSX.Element {
     return (
         <li tabIndex={ 0 } role="listitem" className="w-full max-w-xl md:max-w-none h-full mx-auto relative bg-white border border-[var(--border)] rounded-xl overflow-hidden transition hover:border-[var(--primary)] focus-visible:outline-2 focus-visible:outline-[var(--primary)]">
             <div className="relative group">
@@ -22,13 +22,18 @@ export default function ProjectCard({ src, alt, project, categories, title, desc
                     <p className="text-2xl md:text-3xl font-semibold text-center text-[var(--background)]">{ project }</p>
                 </div>
             </div>
-            <div className="p-8 pt-4 pb-20 border-t border-[var(--border)]">
-                <div className="flex flex-wrap items-center gap-x-2 md:gap-x-4 gap-y-2">
-                    { categories.map((category, index) => <span key={ `Category ${index} - ${category}` } className="px-6 py-0.5 text-xs md:text-sm text-[var(--background)] bg-[var(--primary)]/75 whitespace-nowrap rounded-full">{ category }</span>) }
-                </div>
-                <p className="w-fit mt-6 flex items-center gap-2 font-semibold">{ title }</p>
+            <div className="px-8 pt-6 pb-18 md:pb-20 border-t border-[var(--border)]">
+                <p className="w-fit flex items-center gap-2 font-semibold">{ title }</p>
                 <p className="mt-2">{ description }</p>
-                <div className="absolute right-8 bottom-8 flex items-center justify-end gap-4">
+                <div className="mt-6 flex flex-wrap items-center gap-x-2 gap-y-4">
+                    { stack.map(({ name, logo, small }: { name: string, logo: string, small?: boolean }, index) => (
+                        <div key={`Stack ${index} - ${name}`} className="px-3 py-1 flex items-center gap-2 bg-gray-100 rounded-lg">
+                            <Image src={ logo } alt={ name } width={ small ? 16 : 20 } height={ small ? 16 : 20 } className={ small ? 'w-4 h-4' : 'w-5 h-5' } />
+                            <span className="text-xs md:text-sm font-medium text-gray-700">{ name }</span>
+                        </div>
+                    )) }
+                </div>
+                <div className="absolute right-8 bottom-6 flex items-center justify-end gap-4">
                     <Link href={ live } title={ 'Visit ' + project } target="_blank" rel="noopener noreferrer" className="w-fit flex items-center gap-2 text-[var(--primary)] font-semibold hover:underline focus-visible:outline-none focus-visible:underline focus-visible:decoration-2 focus-visible:decoration-[var(--primary)]">
                         Visit site
                     </Link>
